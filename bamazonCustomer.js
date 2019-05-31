7e645ae52923ce98edf82380d01e989acb96fb7c
Scott <clearplaid@gmail.com>
2019-05-31 14:38:52 -0400

    added customer prompt in customer.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,27 +12,44 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("id: " + connection.threadId);
-    afterConnection();
+    displayProducts();
+
 });
 
-function afterConnection() {
+// display all of the items available for sale. Include the ids, names, and prices of products for sale.
+function displayProducts() {
     connection.query("SELECT * FROM products",function(err, res){
         if (err) throw err;
         console.table(res);
-        connection.end();
+        customerPrompt();
     })
 }
 
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
 // The app should then prompt users with two messages.
+function customerPrompt(){
+inquirer.prompt ([
+    {
+        // The first should ask them the ID of the product they would like to buy.
+        name: "productId",
+        type: "input",
+        message: "What is the product you would like to purchase?"
+    },
+    {
+       // The second message should ask how many units of the product they would like to buy.
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?" 
+    }]).then(function(answer){
+        // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
 
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
+        // However, if your store does have enough of the product, you should fulfill the customer's order.
+        // This means updating the SQL database to reflect the remaining quantity.
 
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
+        // Once the update goes through, show the customer the total cost of their purchase.
+
+    })
+    connection.end();
+}
+
